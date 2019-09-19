0beac14bb30b0bd43b708699a4ea986644f50bce
Saad el din <as704245@iutnice.unice.fr>
2019-09-19 14:43:46 +0200

    Rajout de comms et methode pour rajouter reponses

--- NGCC/src/config/Question.java
+++ NGCC/src/config/Question.java
@@ -1,28 +1,63 @@
 package config;
+
 import java.util.ArrayList;
 
-public  class Question {
-private String titre;
-private boolean multiple=false;
-private ArrayList<Reponse> reponses = new ArrayList<Reponse>();
+public class Question {
+	private String titre; // intitulé de la question
+	private boolean multiple = false; // boolean pour savoir si c'est une question à choix multiple
+	private ArrayList<Reponse> reponses = new ArrayList<Reponse>(); // liste des reponses
 
-public ArrayList<Reponse> getReponses() {
-	return reponses;
-}
-public void setReponses(ArrayList<Reponse> reponses) {
-	this.reponses = reponses;
-}
-public String getTitre() {
-	return titre;
-}
-public void setTitre(String titre) {
-	this.titre = titre;
-}
-public boolean isMultiple() {
-	return multiple;
-}
-public void setMultiple(boolean multiple) {
-	this.multiple = multiple;
-}
+	public Question(String t, boolean b) {
+		titre = t;
+		multiple = b;
+	}
+
+	public ArrayList<Reponse> getReponses() {
+		return reponses;
+	}
+
+	public void setReponses(ArrayList<Reponse> reponses) {
+		this.reponses = reponses;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public boolean isMultiple() {
+		return multiple;
+	}
+
+	public void setMultiple(boolean multiple) {
+		this.multiple = multiple;
+	}
+
+//rajout des reponses à la question
+//prend en parametre un string qui commence par + ou -
+//si le string commence pas par + ou - alors c'est la suite du titre
+	public void addReponse(String ligne) {
+		switch (ligne.substring(0, 1)) {
+		case "+":
+			// reponse correcte
+			reponses.add(new Reponse(ligne.substring(2, ligne.length()), true));
+
+			break;
+
+		case "-":
+			// reponse fausse
+			reponses.add(new Reponse(ligne.substring(2, ligne.length()), false));
+			break;
+
+		default:
+			// si pas + ou - alors c'est la suite à la ligne du titre
+			titre = (titre + "\n" + ligne);
+		}
+	}
+
+// TODO : methode pour verifier si la question a au moins une bonne reponse ET une mauvaise reponse
 
 }
