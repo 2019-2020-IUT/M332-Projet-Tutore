63d5344b32958049d4f29792b699edb78b6131f3
Yessine <yessinebenelbey0305@gmail.com>
2019-09-19 14:53:47 +0200

    Test pour la classe config

---
+++ NGCC/tests/config/TestConfig.java
@@ -0,0 +1,25 @@
+package config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestConfig {
+
+	@Test
+	void testMakeQuestion() {
+		Config c=new Config("test");
+		Question q=new Question("Je suis le titre",false);
+		q=c.makeQuestion("* Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?");
+		assertEquals("Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?",q.getTitre());
+		
+		Question q2=new Question("Selon la serie diffusée en 1991 sur TF1, où le petit Nicolas doit il travailler et s'appliquer ?",false);
+		q2=c.makeQuestion("** Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?");
+		assertEquals("Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?",q2.getTitre());
+		
+		Question q3=new Question("Quelles sont les bonnes réponses",false);
+		q3=c.makeQuestion("*<lines=1> Cette jeune fille vient d'emménager à Sunnydale avec sa mère et rencontre son nouvel observateur. Quel est le nom de ce dernier ?");
+		assertEquals("Cette jeune fille vient d'emménager à Sunnydale avec sa mère et rencontre son nouvel observateur. Quel est le nom de ce dernier ?",q3.getTitre());
+		
+	}
+}
