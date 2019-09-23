e8f5de695dd940065ef5c72bc0e505b8c4b91fdd
Hugo <hugo.poissonnier@etu.unice.fr>
2019-09-23 10:20:28 +0200

    Ajout des méthodes concernant l'ocr(reste à remplir la fonction qui lit les images

---
+++ NGCC/src/ocr_orm/OCR.java
@@ -0,0 +1,18 @@
+package ocr_orm;
+import java.awt.image.BufferedImage;
+import java.io.File; 
+
+import net.sourceforge.tess4j.Tesseract; 
+import net.sourceforge.tess4j.TesseractException; 
+  
+public class OCR {
+	
+	
+
+	public String getOCR(BufferedImage img) {
+		//FAIRE L'OCR
+		String str="";
+	        
+	    return str;
+	}
+}
