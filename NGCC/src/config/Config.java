2b922a3dec6d3e6b34faa734e5adf8152f52d15a
JunkJumper <jose.srifi@gmail.com>
2019-10-09 09:18:08 +0200

    fix non UTF chars

--- NGCC/src/config/Config.java
+++ NGCC/src/config/Config.java
@@ -30,38 +30,38 @@ public class Config {
 	public Config(String s) {
 		// Constructeur, prend en parametre le chemin vers le fichier source
 		source = s;
-		// Initialisation des parametres avec les valeurs par d�faut.
-		// Les �lements avec des placeholders en valeur sont des �lements qui ne servent
+		// Initialisation des parametres avec les valeurs par défaut.
+		// Les élements avec des placeholders en valeur sont des élements qui ne servent
 		// pas pour le moment
 		param.put("PaperSize", "A4"); // A3 A4 A5 letter
 		param.put("Title", "Placeholder"); // titre de l exam
 		param.put("Presentation", "Placeholder"); // texte de consignes
-		param.put("DocumentModel", "PlaceHolder"); // nom du fichier du mod�le
-		param.put("ShuffleQuestions", "1"); // 1 = qt m�lang�es, 0 = non m�l
-		param.put("ShuffleAnswers", "1"); // 1= proposition r�p m�lang�es, 0= non
-		param.put("Code", "8"); // code �tudiant = 8 chiffres (entre 1 et 16)
-		param.put("MarkFormat", "20/4"); // expl "20/4" pour des notes entre 0 et 20 not�es � 0.25 points
-		param.put("NameField", "Nom et Pr�nom"); // remplace le texte
+		param.put("DocumentModel", "PlaceHolder"); // nom du fichier du modéle
+		param.put("ShuffleQuestions", "1"); // 1 = qt mélangées, 0 = non mél
+		param.put("ShuffleAnswers", "1"); // 1= proposition rép mélangées, 0= non
+		param.put("Code", "8"); // code étudiant = 8 chiffres (entre 1 et 16)
+		param.put("MarkFormat", "20/4"); // expl "20/4" pour des notes entre 0 et 20 notées é 0.25 points
+		param.put("NameField", "Nom et Prénom"); // remplace le texte
 		param.put("StudentField",
-				"Veuillez coder votre num�ro\r\n d��tudiant ci-contre et �crire votre nom \r\n dans la case ci-dessous");
-		// sert � remplacer le petit texte qui demande de coder son num�ro d��tudiant et
+				"Veuillez coder votre numéro\r\n d'étudiant ci-contre et écrire votre nom \r\n dans la case ci-dessous");
+		// sert à remplacer le petit texte qui demande de coder son numéro déétudiant et
 		// inscrire son nom
-		param.put("MarkField", "Veuillez coder le num�ro de l'�tudiant");
-		param.put("SeparateAnswerSheet", "1"); // si 1 = feuille de r�ponse s�par�e.
-		param.put("AnswerSheetTitle", "Title"); // titre � inscrire en tete de la feuille de r�p
-		param.put("AnswerSheetPresentation", "Presentation"); // Donne le texte de pr�sentation de la feuille de r�ponse
+		param.put("MarkField", "Veuillez coder le numéro de l'étudiant");
+		param.put("SeparateAnswerSheet", "1"); // si 1 = feuille de réponse séparée.
+		param.put("AnswerSheetTitle", "Title"); // titre é inscrire en tete de la feuille de rép
+		param.put("AnswerSheetPresentation", "Presentation"); // Donne le texte de présentation de la feuille de réponse
 		param.put("SingleSided", "Placeholder");// si valeur = 1, aucune page blanche entre feuille de sujet et de
-												// r�ponse
-		param.put("DefaultScoringS", "Placeholder");// Donne le bar�me par d�faut pour les questions simples
-		param.put("DefaultScoringM", "Placeholder");// Donne le bar�me par d�faut pour les questions � choix multiple
-		param.put("QuestionBlocks", "Placeholder");// prend 0 pour valeur pour permettre � la boite d'une question boite
-													// d'etre coup� sur plusieurs pages, prend 1 sinon
+												// réponse
+		param.put("DefaultScoringS", "Placeholder");// Donne le baréme par défaut pour les questions simples
+		param.put("DefaultScoringM", "Placeholder");// Donne le baréme par défaut pour les questions é choix multiple
+		param.put("QuestionBlocks", "Placeholder");// prend 0 pour valeur pour permettre é la boite d'une question boite
+													// d'etre coupé sur plusieurs pages, prend 1 sinon
 
 	}
 
 	public void readConfig() {
 		// Methode pour lire le fichier config en chemin dans la variable source
-		// Si une ligne du fichier correspond � un parametre, changer la valeur du
+		// Si une ligne du fichier correspond é un parametre, changer la valeur du
 		// parametre avec celle dans le fichier (si valeur valide)
 		// Gere aussi les questions dans le fichier source et les mets dans une liste de
 		// questions.
@@ -72,7 +72,7 @@ public class Config {
 			Question q;
 			ligne = scan.nextLine();
 			// ligne pour gerer le code FEFF en UTF-8 BOM qui peut apparaitre si le fichier
-			// txt est edit� avec windows notepad
+			// txt est edité avec windows notepad
 			// ce caractere apparait uniquement en debut de fichier
 			if (ligne.startsWith("\uFEFF"))
 				ligne = ligne.substring(1);
@@ -86,7 +86,7 @@ public class Config {
 					{
 						q = makeQuestion(ligne);
 						ligne = scan.nextLine(); // on scan la prochaine ligne
-						while (!ligne.equals("")) // tant que la ligne n'est pas vide, on lit la suite qui est suppos�
+						while (!ligne.equals("")) // tant que la ligne n'est pas vide, on lit la suite qui est supposé
 													// etre les reponses
 						{
 							q.addReponse(ligne);
@@ -109,7 +109,7 @@ public class Config {
 	}
 
 	// methode pour creer une question
-	// methode utilis�e � partir d'un string suppos� lu sur un fichier config
+	// methode utilisée é partir d'un string supposé lu sur un fichier config
 	// TODO : gestion des options telles que coeff et frozenanswer
 	public Question makeQuestion(String ligne) {
 		Question q;
@@ -117,7 +117,7 @@ public class Config {
 		switch (s) {
 
 		case "*":
-			// si c'est une * alors c'est une question � choix multiple
+			// si c'est une * alors c'est une question é choix multiple
 			q = new Question(ligne.substring(3, ligne.length()), true);
 
 			break;
@@ -126,14 +126,14 @@ public class Config {
 			// si c'est une < alors c'est une question de type boite
 			int debut = ligne.indexOf("="); // on cherche la position du =, le caractere apres le = sera le nb de lignes
 											// de la boite
-			int fin = ligne.indexOf(">"); // on cherce la position du >, la suite de ce caractere sera l'intitul� de la
+			int fin = ligne.indexOf(">"); // on cherce la position du >, la suite de ce caractere sera l'intitulé de la
 											// question
 			int nblignes = Integer.parseInt(ligne.substring(debut + 1, fin));
 			q = new QuestionBoite(ligne.substring(fin + 2, ligne.length()), false, nblignes);
 			break;
 
 		default:
-			// si pas une des conditions cit�es en haut, alors c'est une question � choix
+			// si pas une des conditions citées en haut, alors c'est une question é choix
 			// unique
 			q = new Question(ligne.substring(2, ligne.length()), false);
 		}
@@ -141,7 +141,7 @@ public class Config {
 	}
 
 	// modification des valeurs du hashmap param
-	// lecture d'un string suppos� lu sur un fichier config
+	// lecture d'un string supposé lu sur un fichier config
 	public void lireParam(String s) {
 		int n = s.indexOf(":"); // recherche de position du premier ":" pour pouvoir separer le nom du param de
 								// sa valeur
@@ -149,7 +149,7 @@ public class Config {
 		while (spl[1].substring(0, 1).equals(" "))
 			spl[1] = spl[1].substring(1, spl[1].length());
 		spl[0] = spl[0].toUpperCase(); // pour eviter la casse, on met tout en upper case
-		switch (spl[0]) // chaque case correspond � un parametre, pour le moment on ignore tout
+		switch (spl[0]) // chaque case correspond é un parametre, pour le moment on ignore tout
 						// parametre qui n'est pas utile au programme.
 		{
 		case "PAPERSIZE":
@@ -195,16 +195,16 @@ public class Config {
 			setAnswerSheetPresentation(spl[1]);
 
 			break;
-		default: // parametre mal tap� ou non utile (pour le moment) au programme, on l'ignore
+		default: // parametre mal tapé ou non utile (pour le moment) au programme, on l'ignore
 		}
 	}
 
 	// liste des set de chaque valeur de parametre
-	// actuellement, uniquement les parametres de l'�tape 1 sont trait�s
+	// actuellement, uniquement les parametres de l'étape 1 sont traités
 
 	// TODO
-	// possibilit� d'afficher sur la console messages de valeur invalide et valeur
-	// par d�faut utilis�e en cas d'erreur si verbose
+	// possibilité d'afficher sur la console messages de valeur invalide et valeur
+	// par défaut utilisée en cas d'erreur si verbose
 	public void setPaperSize(String s) {
 		s = s.toUpperCase();
 		s = s.trim();
