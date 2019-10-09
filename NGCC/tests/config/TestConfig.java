76a13571274a5e39a5a27b097d9632598feb557e
JunkJumper <jose.srifi@gmail.com>
2019-10-09 09:19:06 +0200

    fix non UTF8 chars

--- NGCC/tests/config/TestConfig.java
+++ NGCC/tests/config/TestConfig.java
@@ -13,13 +13,13 @@ public class TestConfig {
 		q=c.makeQuestion("* Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?");
 		assertEquals("Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?",q.getTitre());
 		
-		Question q2=new Question("Selon la serie diffus�e en 1991 sur TF1, o� le petit Nicolas doit il travailler et s'appliquer ?",false);
+		Question q2=new Question("Selon la serie diffusée en 1991 sur TF1, où le petit Nicolas doit il travailler et s'appliquer ?",false);
 		q2=c.makeQuestion("** Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?");
 		assertEquals("Dans M.A.S.K. qui sont les pilotes ou copilote de Rhino (un camion tracteur Kenworth w900)?",q2.getTitre());
 		
-		Question q3=new Question("Quelles sont les bonnes r�ponses",false);
-		q3=c.makeQuestion("*<lines=1> Cette jeune fille vient d'emm�nager � Sunnydale avec sa m�re et rencontre son nouvel observateur. Quel est le nom de ce dernier ?");
-		assertEquals("Cette jeune fille vient d'emm�nager � Sunnydale avec sa m�re et rencontre son nouvel observateur. Quel est le nom de ce dernier ?",q3.getTitre());
+		Question q3=new Question("Quelles sont les bonnes réponses",false);
+		q3=c.makeQuestion("*<lines=1> Cette jeune fille vient d'emménager à Sunnydale avec sa mère et rencontre son nouvel observateur. Quel est le nom de ce dernier ?");
+		assertEquals("Cette jeune fille vient d'emménager à Sunnydale avec sa mère et rencontre son nouvel observateur. Quel est le nom de ce dernier ?",q3.getTitre());
 		
 	}
 }
