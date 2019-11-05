4540beb17d7c2ef13225ce8380179d62947b47e5
Hugo Nortier <hugo.nortier@etu.unice.fr>
2019-11-05 11:55:40 +0100

    #9 ajout classe groupe

---
+++ src/config/Groupe.java
@@ -0,0 +1,25 @@
+package config;
+
+import java.util.ArrayList;
+
+public class Groupe {
+	private ArrayList<Question> questions = new ArrayList<Question>();
+	private int nbQtChoisie;
+	
+	public Groupe(int nbQtChoisie) {
+		this.nbQtChoisie = nbQtChoisie;
+	}
+	
+/*
+
+Groupes de sélections aléatoires:
+Il est possible que certaines questions soient rassemblées dans un groupe de sélections aléatoires. 
+Le principe de ce type de groupe est de pouvoir sélectionner n questions parmi les p questions du groupe
+(0<n<p).
+
+*/
+
+	
+	
+	
+}
