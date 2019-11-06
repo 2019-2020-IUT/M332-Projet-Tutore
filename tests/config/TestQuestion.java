55ff923f99a49eee9d0b6b52d6ecf61f958cf906
by801687 <by801687@iutnice.unice.fr>
2019-11-06 11:29:49 +0100

    #9 test verifQuestion

--- tests/config/TestQuestion.java
+++ tests/config/TestQuestion.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class TestQuestion {
 	@Test
-	void testQuestion() {
+	public void testQuestion() {
 		Question q=new Question("Je suis le titre",false);
 		 assertEquals("Je suis le titre",q.getTitre());
 		 assertEquals(false,q.isMultiple());
@@ -19,10 +19,10 @@ public class TestQuestion {
 		 assertEquals(true,q2.isMultiple());
 	}
 	@Test
-	void testAddReponse() {
-		Reponse r1=new Reponse("+ Au collège des ninjas",true);
+	public void testAddReponse() {
+		Reponse r1=new Reponse("Au collège des ninjas",true);
 		Question q=new Question("Selon la serie diffusée en 1991 sur TF1, où le petit Nicolas doit il travailler et s'appliquer ?",false);
-		q.addReponse(r1.getIntitule());
+		q.addReponse("+ "+r1.getIntitule());
 		assertEquals("Au collège des ninjas",q.getReponses().get(0).getIntitule());
 		Reponse r2=new Reponse("- A l'académie des ninjas",false);
 		q.addReponse(r2.getIntitule());
@@ -31,5 +31,27 @@ public class TestQuestion {
 		assertEquals("Selon la serie diffusée en 1991 sur TF1, où le petit Nicolas doit il travailler et s'appliquer ?\nSuite de la question",q.getTitre());
 		
 	}
+	@Test
+	public void testVerifQuestion() {
+		Question q=new Question("D'après le générique de 1978, jusqu'où bondit le merveilleux génie ?",false);
+		Reponse r1=new Reponse("Jupiter",true);
+		Reponse r2=new Reponse("La Terre",false);
+		Reponse r3=new Reponse("Le fond de l'univers",false);
+		Reponse r4=new Reponse("Neugebauer",false);
+		q.addReponse("+ "+r1.getIntitule());
+		q.addReponse("- "+r2.getIntitule());
+		q.addReponse("- "+r3.getIntitule());
+		q.addReponse("- "+r4.getIntitule());
+		assertTrue(q.verifQuestion());
+		Question q2=new Question("D'après le générique de 1978, jusqu'où bondit le merveilleux génie ?",false);
+		q2.addReponse("- "+r1.getIntitule());
+		q2.addReponse("- "+r2.getIntitule());
+		q2.addReponse("- "+r3.getIntitule());
+		q2.addReponse("- "+r4.getIntitule());
+		assertFalse(q2.verifQuestion());
+				
+		
+	}
+	
 	
 }
